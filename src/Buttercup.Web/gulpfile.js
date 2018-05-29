10071b6a90d8cdf4c443c850b8fdede12f41ef0b
Stephen Feest <steve84@gmail.com>
2018-05-29 22:03:02 +0100

    Enable style sheet linting

--- src/Buttercup.Web/gulpfile.js
+++ src/Buttercup.Web/gulpfile.js
@@ -1,17 +1,26 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const lesshint = require('gulp-lesshint');
 const sourcemaps = require('gulp-sourcemaps');
 const watchLess = require('gulp-watch-less');
 
 const paths = {};
-paths.lessEntryPoint = `${__dirname}/styles/main.less`;
+paths.styles = `${__dirname}/styles`;
+paths.lessEntryPoint = `${paths.styles}/main.less`;
 paths.styleAssets = `${__dirname}/wwwroot/assets/styles`;
 
 gulp.task('default', ['build']);
 
-gulp.task('build', ['build:styles']);
+gulp.task('build', ['build:styles', 'lint:styles']);
 
 gulp.task('build:styles', () => bundleStyles(gulp.src(paths.lessEntryPoint)));
+
+gulp.task('lint', ['lint:styles']);
+
+gulp.task('lint:styles', () => gulp.src(`${paths.styles}/*.less`)
+  .pipe(lesshint())
+  .pipe(lesshint.reporter())
+  .pipe(lesshint.failOnError()));
 
 gulp.task('watch', ['watch:styles']);
 
