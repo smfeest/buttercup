a489d72f521695ab3b9c11303050d66e490efdea
Stephen Feest <steve84@gmail.com>
2018-05-29 23:46:19 +0100

    Add method for registering data access services

---
+++ test/Buttercup.DataAccess.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using MySql.Data.MySqlClient;
+using Xunit;
+
+namespace Buttercup.DataAccess
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        #region AddDataAccessServices
+
+        [Fact]
+        public void AddDataAccessServicesAddsConnectionSource()
+        {
+            var mockServiceCollection = new Mock<IServiceCollection>();
+
+            Func<IServiceProvider, object> instanceFactory = null;
+
+            mockServiceCollection
+                .Setup(x => x.Add(It.Is<ServiceDescriptor>(serviceDescriptor =>
+                    serviceDescriptor.ServiceType == typeof(IDbConnectionSource) &&
+                    serviceDescriptor.Lifetime == ServiceLifetime.Transient)))
+                .Callback<ServiceDescriptor>(serviceDescriptor =>
+                    instanceFactory = serviceDescriptor.ImplementationFactory).Verifiable();
+
+            mockServiceCollection.Object.AddDataAccessServices("sample-connection-string");
+
+            mockServiceCollection.Verify();
+
+            var connectionSource = (DbConnectionSource)instanceFactory(null);
+
+            Assert.Equal("sample-connection-string", connectionSource.ConnectionString);
+        }
+
+        #endregion
+    }
+}
