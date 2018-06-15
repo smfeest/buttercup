0cd83d80a312128b0f76cfb870edd106df0dbb47
Stephen Feest <steve84@gmail.com>
2018-06-15 18:54:44 +0100

    Add user model

---
+++ src/Buttercup.Models/User.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace Buttercup.Models
+{
+    /// <summary>
+    /// Represents a user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the user ID.
+        /// </summary>
+        /// <value>
+        /// The user ID.
+        /// </value>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address.
+        /// </summary>
+        /// <value>
+        /// The email address.
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hashed password.
+        /// </summary>
+        /// <value>
+        /// The hashed password.
+        /// </value>
+        public string HashedPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time at which the record was created.
+        /// </summary>
+        /// <value>
+        /// The date and time at which the record was created.
+        /// </value>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time at which the record was last modified.
+        /// </summary>
+        /// <value>
+        /// The date and time at which the record was last modified.
+        /// </value>
+        public DateTime Modified { get; set; }
+
+        /// <summary>
+        /// Gets or sets the revision number.
+        /// </summary>
+        /// <value>
+        /// The revision number.
+        /// </value>
+        public int Revision { get; set; }
+    }
+}
