44478e612d6d09c36ac9ffbd72cce6436dc68884
Stephen Feest <steve84@gmail.com>
2018-06-18 23:45:15 +0100

    Add email sender

--- src/Buttercup.Email/EmailOptions.cs
+++ src/Buttercup.Email/EmailOptions.cs
@@ -12,5 +12,13 @@ namespace Buttercup.Email
         /// The SendGrid API key.
         /// </value>
         public string ApiKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the from address.
+        /// </summary>
+        /// <value>
+        /// The from address.
+        /// </value>
+        public string FromAddress { get; set; }
     }
 }
---
+++ test/Buttercup.Email.Tests/EmailSenderTests.cs
@@ -0,0 +1,98 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Moq;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Xunit;
+
+namespace Buttercup.Email
+{
+    public class EmailSenderTests
+    {
+        #region Send
+
+        [Fact]
+        public async Task SendSetsFromAddress()
+        {
+            var context = new Context(
+                new EmailOptions { FromAddress = "buttercup@example.com" });
+
+            await context.EmailSender.Send(
+                "sample.recipient@example.com", "Sample subject", "Sample body");
+
+            Assert.Equal("buttercup@example.com", context.SentMessage.From.Email);
+        }
+
+        [Fact]
+        public async Task SendSetsToAddress()
+        {
+            var context = new Context();
+
+            await context.EmailSender.Send(
+                "sample.recipient@example.com", "Sample subject", "Sample body");
+
+            var personalization = Assert.Single(context.SentMessage.Personalizations);
+            var to = Assert.Single(personalization.Tos);
+            Assert.Equal("sample.recipient@example.com", to.Email);
+        }
+
+        [Fact]
+        public async Task SendSetsSubject()
+        {
+            var context = new Context();
+
+            await context.EmailSender.Send(
+                "sample.recipient@example.com", "Sample subject", "Sample body");
+
+            Assert.Equal("Sample subject", context.SentMessage.Subject);
+        }
+
+        [Fact]
+        public async Task SendSetsBody()
+        {
+            var context = new Context();
+
+            await context.EmailSender.Send(
+                "sample.recipient@example.com", "Sample subject", "Sample body");
+
+            Assert.Equal("Sample body", context.SentMessage.PlainTextContent);
+        }
+
+        #endregion
+
+        private class Context
+        {
+            public Context(EmailOptions emailOptions = null)
+            {
+                this.EmailSender = new EmailSender(
+                    new FakeSendGridClientAccessor(this.MockSendGridClient.Object),
+                    Options.Create(emailOptions ?? new EmailOptions()));
+
+                this.MockSendGridClient
+                    .Setup(
+                        x => x.SendEmailAsync(It.IsAny<SendGridMessage>(), CancellationToken.None))
+                    .Returns((SendGridMessage message, CancellationToken cancellationToken) =>
+                    {
+                        this.SentMessage = message;
+
+                        return Task.FromResult<Response>(null);
+                    });
+            }
+
+            public EmailSender EmailSender { get; }
+
+            public Mock<ISendGridClient> MockSendGridClient { get; } = new Mock<ISendGridClient>();
+
+            public SendGridMessage SentMessage { get; private set; }
+        }
+
+        private class FakeSendGridClientAccessor : ISendGridClientAccessor
+        {
+            public FakeSendGridClientAccessor(ISendGridClient sendGridClient) =>
+                this.SendGridClient = sendGridClient;
+
+            public ISendGridClient SendGridClient { get; }
+        }
+    }
+}
