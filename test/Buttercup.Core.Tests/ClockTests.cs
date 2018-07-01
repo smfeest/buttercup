bc34315b3cc1676176ce789f3951e030fac3ac6c
Stephen Feest <steve84@gmail.com>
2018-07-01 11:30:46 +0100

    Add clock service
    Abstracting the `DateTime.UtcNow` property into a service makes it
    easier to unit test methods that use the current time.

---
+++ test/Buttercup.Core.Tests/ClockTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Buttercup
+{
+    public class ClockTests
+    {
+        #region UtcNow
+
+        [Fact]
+        public void UtcNowReturnsCurrentUtcTime()
+        {
+            var start = DateTime.UtcNow;
+            var actual = new Clock().UtcNow;
+            var finish = DateTime.UtcNow;
+
+            Assert.InRange(actual, start, finish);
+        }
+
+        #endregion
+    }
+}
