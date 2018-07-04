5f663bd3ad1b880f6024e7c6b537ce82945ab5ea
Stephen Feest <steve84@gmail.com>
2018-07-04 21:06:33 +0100

    Add reset password views and controller actions

--- test/Buttercup.Web.Tests/Controllers/AuthenticationControllerTests.cs
+++ test/Buttercup.Web.Tests/Controllers/AuthenticationControllerTests.cs
@@ -73,6 +73,111 @@ namespace Buttercup.Web.Controllers
 
         #endregion
 
+        #region ResetPassword (GET)
+
+        [Fact]
+        public async Task ResetPasswordGetReturnsDefaultViewResultWhenTokenIsValid()
+        {
+            using (var context = new Context())
+            {
+                context.MockAuthenticationManager
+                    .Setup(x => x.PasswordResetTokenIsValid("sample-token"))
+                    .ReturnsAsync(true);
+
+                var result = await context.AuthenticationController.ResetPassword("sample-token");
+
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Null(viewResult.ViewName);
+            }
+        }
+
+        [Fact]
+        public async Task ResetPasswordGetReturnsInvalidTokenViewResultWhenTokenIsInvalid()
+        {
+            using (var context = new Context())
+            {
+                context.MockAuthenticationManager
+                    .Setup(x => x.PasswordResetTokenIsValid("sample-token"))
+                    .ReturnsAsync(false);
+
+                var result = await context.AuthenticationController.ResetPassword("sample-token");
+
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Equal("ResetPasswordInvalidToken", viewResult.ViewName);
+            }
+        }
+
+        #endregion
+
+        #region ResetPassword (POST)
+
+        [Fact]
+        public async Task ResetPasswordPostReturnsDefaultViewResultWhenModelIsInvalid()
+        {
+            using (var context = new Context())
+            {
+                context.AuthenticationController.ModelState.AddModelError("test", "test");
+
+                var viewModel = new ResetPasswordViewModel();
+                var result = await context.AuthenticationController.ResetPassword(
+                    "sample-token", viewModel);
+
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Same(viewModel, viewResult.Model);
+            }
+        }
+
+        [Fact]
+        public async Task ResetPasswordPostReturnsInvalidTokenViewResultWhenTokenIsInvalid()
+        {
+            using (var context = new Context())
+            {
+                context.MockAuthenticationManager
+                    .Setup(x => x.ResetPassword("sample-token", "sample-password"))
+                    .ThrowsAsync(new InvalidTokenException());
+
+                var result = await context.AuthenticationController.ResetPassword(
+                    "sample-token", new ResetPasswordViewModel { Password = "sample-password" });
+
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Equal("ResetPasswordInvalidToken", viewResult.ViewName);
+            }
+        }
+
+        [Fact]
+        public async Task ResetPasswordPostSignsInUser()
+        {
+            using (var context = new Context())
+            {
+                var user = new User();
+
+                context.MockAuthenticationManager
+                    .Setup(x => x.ResetPassword("sample-token", "sample-password"))
+                    .ReturnsAsync(user);
+
+                await context.AuthenticationController.ResetPassword(
+                    "sample-token", new ResetPasswordViewModel { Password = "sample-password" });
+
+                context.MockAuthenticationManager.Verify(x => x.SignIn(context.HttpContext, user));
+            }
+        }
+
+        [Fact]
+        public async Task ResetPasswordPostRedirectsToHomeIndex()
+        {
+            using (var context = new Context())
+            {
+                var result = await context.AuthenticationController.ResetPassword(
+                    "sample-token", new ResetPasswordViewModel());
+
+                var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+                Assert.Equal("Home", redirectResult.ControllerName);
+                Assert.Equal(nameof(HomeController.Index), redirectResult.ActionName);
+            }
+        }
+
+        #endregion
+
         #region SignIn (GET)
 
         [Fact]
