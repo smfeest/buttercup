0d8d1cd5a0e280fbceb05713381d3a66947ff090
Stephen Feest <steve84@gmail.com>
2019-01-08 20:50:08 +0000

    Add method for converting time zone options to select list items

---
+++ test/Buttercup.Web.Tests/Localization/TimeZoneOptionCollectionExtensionsTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Buttercup.Web.Localization
+{
+    public class TimeZoneOptionCollectionExtensionsTests
+    {
+        #region AsSelectListItems
+
+        public void AsSelectListItemsConvertsTimeZoneOptionsToSelectListItems()
+        {
+            var timeZoneOption = new TimeZoneOption(
+                "Sample/Time_Zone", TimeSpan.Zero, "Sample-Offset", "Sample-City");
+
+            var selectListItem = new[] { timeZoneOption }.AsSelectListItems().First();
+
+            Assert.Equal(selectListItem.Value, timeZoneOption.Id);
+            Assert.Equal(selectListItem.Text, timeZoneOption.Description);
+        }
+
+        #endregion
+    }
+}
