656beb7747b7996897ae8f483577397550be48ec
Stephen Feest <steve84@gmail.com>
2022-01-30 14:05:36 +0000

    Add access token serializer

---
+++ src/Buttercup.Web/Authentication/IAccessTokenSerializer.cs
@@ -0,0 +1,29 @@
+namespace Buttercup.Web.Authentication;
+
+/// <summary>
+/// Defines the contract for the access token serializer.
+/// </summary>
+public interface IAccessTokenSerializer
+{
+    /// <summary>
+    /// Serializes an access token's payload.
+    /// </summary>
+    /// <param name="payload">
+    /// The payload.
+    /// </param>
+    /// <returns>
+    /// The serialized payload.
+    /// </returns>
+    byte[] Serialize(AccessTokenPayload payload);
+
+    /// <summary>
+    /// Deserializes an access token's payload.
+    /// </summary>
+    /// <param name="data">
+    /// The serialized payload.
+    /// </param>
+    /// <returns>
+    /// The deserialized payload.
+    /// </returns>
+    AccessTokenPayload Deserialize(byte[] data);
+}
--- src/Buttercup.Web/Program.cs
+++ src/Buttercup.Web/Program.cs
@@ -68,6 +68,7 @@ services
 
 services
     .AddTransient<IPasswordHasher<User?>, PasswordHasher<User?>>()
+    .AddTransient<IAccessTokenSerializer, AccessTokenSerializer>()
     .AddTransient<IAuthenticationMailer, AuthenticationMailer>()
     .AddTransient<IAuthenticationManager, AuthenticationManager>()
     .AddTransient<CookieAuthenticationEventsHandler>()
