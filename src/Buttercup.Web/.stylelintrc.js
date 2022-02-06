59db7ed4182caa9fe7c7ec07b7f6ab76c6f4b9be
Stephen Feest <steve84@gmail.com>
2022-02-06 20:41:58 +0000

    Migrate to Stylelint 14
    As discussed in the migration guide [1], to use Stylelint 14.0.0 and
    above to lint anything other than regular CSS files we must install the
    appropriate PostCSS syntax (in our case `postcss-less`) and configure
    Stylelint to use it via the `customSyntax` option.
    
    [1] https://stylelint.io/migration-guide/to-14#syntax-option-and-automatic-inferral-of-syntax

--- src/Buttercup.Web/.stylelintrc.js
+++ src/Buttercup.Web/.stylelintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: 'stylelint-config-recommended',
+  customSyntax: 'postcss-less',
   rules: {
     'no-descending-specificity': [true, { ignore: ['selectors-within-list'] }],
   },
