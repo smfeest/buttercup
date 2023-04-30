8b610199ea35320c6a9af2862a1eab52211d0d8f
Stephen Feest <steve84@gmail.com>
2023-04-30 20:49:38 +0100

    Add method for setting up the database context factory
    Auto-detecting the database server version involves a round trip to the
    database server. We're therefore adding the server version to the
    service collection as a singleton so that it can be retrieved through
    the service provider in the database context setup action.

---
+++ src/Buttercup.EntityModel/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Buttercup.EntityModel;
+
+/// <summary>
+/// Extends <see cref="IServiceCollection" /> to facilitate registration of the application database
+/// context factory.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers the application database context factory.
+    /// </summary>
+    /// <param name="services">
+    /// The service collection.
+    /// </param>
+    /// <param name="connectionString">
+    /// The database connection string.
+    /// </param>
+    /// <returns>
+    /// The service collection to allow chaining.
+    /// </returns>
+    public static IServiceCollection AddAppDbContextFactory(
+        this IServiceCollection services, string connectionString) => services
+            .AddSingleton<ServerVersion>(ServerVersion.AutoDetect(connectionString))
+            .AddPooledDbContextFactory<AppDbContext>((serviceProvider, options) => options
+                .UseMySql(connectionString, serviceProvider.GetRequiredService<ServerVersion>()));
+}
