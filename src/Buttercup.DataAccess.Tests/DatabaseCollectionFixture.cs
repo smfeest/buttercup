b2836cf97f0183b354bd0988f3f76a53c897b9ff
Stephen Feest <steve84@gmail.com>
2023-05-02 22:11:07 +0100

    Derive database collection fixture from generic database fixture
    I also experimented with removing the database collection completely and
    having a separate database fixture for each test class instead. However,
    on my local machine, the extra time spent creating separate test
    databases for each class more than cancelled out the time gained from
    being able to run the test classes in parallel.

--- src/Buttercup.DataAccess.Tests/DatabaseCollectionFixture.cs
+++ src/Buttercup.DataAccess.Tests/DatabaseCollectionFixture.cs
@@ -1,7 +1,4 @@
-using Buttercup.EntityModel;
-using Microsoft.EntityFrameworkCore;
-using MySqlConnector;
-using Xunit;
+using Buttercup.TestUtils;
 
 namespace Buttercup.DataAccess;
 
@@ -9,46 +6,6 @@ namespace Buttercup.DataAccess;
 /// A fixture that recreates the test database before tests in <see cref="DatabaseCollection" /> are
 /// executed.
 /// </summary>
-public class DatabaseCollectionFixture : IAsyncLifetime
+public class DatabaseCollectionFixture : DatabaseFixture<DatabaseCollection>
 {
-    private const string Server = "localhost";
-    private const string User = "buttercup_dev";
-    private const string DatabaseName = "buttercup_test";
-
-    private static readonly string connectionString = new MySqlConnectionStringBuilder
-    {
-        Server = Server,
-        UserID = User,
-        Database = DatabaseName,
-    }.ToString();
-
-    private static readonly Lazy<ServerVersion> serverVersion =
-        new(() => ServerVersion.AutoDetect(connectionString));
-
-    /// <summary>
-    /// Creates a new <see cref="AppDbContext" /> that connects to the test database.
-    /// </summary>
-    /// <returns>
-    /// The new <see cref="AppDbContext" />.
-    /// </returns>
-    public AppDbContext CreateDbContext()
-    {
-        var options = new DbContextOptionsBuilder()
-            .UseAppDbOptions(connectionString, serverVersion.Value)
-            .Options;
-
-        return new(options);
-    }
-
-    private async Task RecreateDatabase()
-    {
-        using var dbContext = CreateDbContext();
-
-        await dbContext.Database.EnsureDeletedAsync();
-        await dbContext.Database.EnsureCreatedAsync();
-    }
-
-    Task IAsyncLifetime.InitializeAsync() => this.RecreateDatabase();
-
-    Task IAsyncLifetime.DisposeAsync() => Task.CompletedTask;
 }
