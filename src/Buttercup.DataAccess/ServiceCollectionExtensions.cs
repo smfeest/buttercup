0125333d806d09b905b0687665cec505792755ee
Stephen Feest <stephen.feest@cezannehr.com>
2023-05-07 20:18:49 +0100

    Remove redundant data access services and utilities
    These have become unused as a result of the data access providers and
    tests having been reimplemented using Entity Framework Core instead.

--- src/Buttercup.DataAccess/ServiceCollectionExtensions.cs
+++ src/Buttercup.DataAccess/ServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace Buttercup.DataAccess;
 
@@ -15,43 +13,13 @@ public static class ServiceCollectionExtensions
     /// <param name="services">
     /// The service collection.
     /// </param>
-    /// <param name="configure">
-    /// An action that configures the data access options.
-    /// </param>
     /// <returns>
     /// The service collection to allow chaining.
     /// </returns>
-    public static IServiceCollection AddDataAccessServices(
-        this IServiceCollection services, Action<DataAccessOptions> configure) =>
-        services.AddDataAccessServices(options => options.Configure(configure));
-
-    /// <summary>
-    /// Adds data access services to the service collection.
-    /// </summary>
-    /// <param name="services">
-    /// The service collection.
-    /// </param>
-    /// <param name="configuration">
-    /// The data access configuration.
-    /// </param>
-    /// <returns>
-    /// The service collection to allow chaining.
-    /// </returns>
-    public static IServiceCollection AddDataAccessServices(
-        this IServiceCollection services, IConfiguration configuration) =>
-        services.AddDataAccessServices(options => options.Bind(configuration));
-
-    private static IServiceCollection AddDataAccessServices(
-        this IServiceCollection services,
-        Action<OptionsBuilder<DataAccessOptions>> buildOptionsAction)
-    {
-        buildOptionsAction(services.AddOptions<DataAccessOptions>().ValidateDataAnnotations());
-
-        return services
+    public static IServiceCollection AddDataAccessServices(this IServiceCollection services) =>
+        services
             .AddTransient<IAuthenticationEventDataProvider, AuthenticationEventDataProvider>()
-            .AddTransient<IMySqlConnectionSource, MySqlConnectionSource>()
             .AddTransient<IPasswordResetTokenDataProvider, PasswordResetTokenDataProvider>()
             .AddTransient<IRecipeDataProvider, RecipeDataProvider>()
             .AddTransient<IUserDataProvider, UserDataProvider>();
-    }
 }
