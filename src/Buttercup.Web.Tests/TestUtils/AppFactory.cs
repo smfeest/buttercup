c63e83801c759e07900c8ba4618e3aab2b27451f
Stephen Feest <steve84@gmail.com>
2023-05-19 21:19:41 +0100

    Add base class and fixture for end to end tests

---
+++ src/Buttercup.Web.Tests/TestUtils/AppFactory.cs
@@ -0,0 +1,32 @@
+using Buttercup.TestUtils;
+using Buttercup.Web.Controllers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Buttercup.Web.TestUtils;
+
+/// <summary>
+/// A fixture for bootstrapping the application in memory for end to end tests.
+/// </summary>
+/// <typeparam name="T">
+/// The test class.
+/// </typeparam>
+public class AppFactory<T> : WebApplicationFactory<HomeController>, IAsyncLifetime
+{
+    /// <summary>
+    /// Gets the database fixture.
+    /// </summary>
+    public DatabaseFixture<T> DatabaseFixture { get; } = new();
+
+    /// <inheritdoc/>
+    protected override void ConfigureWebHost(IWebHostBuilder builder) =>
+        builder
+            .UseSetting("ConnectionStrings:AppDb", this.DatabaseFixture.ConnectionString)
+            .UseSetting("Email:ApiKey", "fake-key");
+
+    Task IAsyncLifetime.InitializeAsync() =>
+        ((IAsyncLifetime)this.DatabaseFixture).InitializeAsync();
+
+    Task IAsyncLifetime.DisposeAsync() =>
+        ((IAsyncLifetime)this.DatabaseFixture).DisposeAsync();
+}
