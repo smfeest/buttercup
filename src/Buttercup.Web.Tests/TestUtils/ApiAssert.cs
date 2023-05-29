7c10ba7e66beccf50997e2afef966c6eb4e05acf
Stephen Feest <steve84@gmail.com>
2023-05-29 22:14:04 +0100

    Add method for asserting that an API response contains a specific error

--- src/Buttercup.Web.Tests/TestUtils/ApiAssert.cs
+++ src/Buttercup.Web.Tests/TestUtils/ApiAssert.cs
@@ -10,6 +10,33 @@ namespace Buttercup.Web.TestUtils;
 public static class ApiAssert
 {
     /// <summary>
+    /// Verifies that a response has an errors field containing a single error, with a specific
+    /// error code.
+    /// </summary>
+    /// <param name="expectedErrorCode">
+    /// The expected error code.
+    /// </param>
+    /// <param name="document">
+    /// The response document.
+    /// </param>
+    /// <returns>
+    /// A task for the operation.
+    /// </returns>
+    public static void HasSingleError(string expectedErrorCode, JsonDocument document)
+    {
+        Assert.True(document.RootElement.TryGetProperty("errors", out var errorsElement));
+
+        var actualErrorCode = errorsElement
+            .EnumerateArray()
+            .Single()
+            .GetProperty("extensions")
+            .GetProperty("code")
+            .GetString();
+
+        Assert.Equal(expectedErrorCode, actualErrorCode);
+    }
+
+    /// <summary>
     /// Verifies that a response has a data field and no errors field.
     /// </summary>
     /// <param name="document">
