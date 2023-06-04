b43078dbf28ab3f3b46d88e062fd98e27eecb9dc
Stephen Feest <steve84@gmail.com>
2023-06-04 13:35:16 +0100

    Convert `TimeZoneOption` to a positional record type

--- src/Buttercup.Web.Tests/Localization/TimeZoneOptionTests.cs
+++ src/Buttercup.Web.Tests/Localization/TimeZoneOptionTests.cs
@@ -4,22 +4,6 @@ namespace Buttercup.Web.Localization;
 
 public sealed class TimeZoneOptionTests
 {
-    #region Constructor
-
-    [Fact]
-    public void ConstructorSetsProperties()
-    {
-        var timeZoneOption = new TimeZoneOption(
-            "sample-id", new(1, 2, 3), "sample-offset", "sample-city");
-
-        Assert.Equal("sample-id", timeZoneOption.Id);
-        Assert.Equal(new(1, 2, 3), timeZoneOption.CurrentOffset);
-        Assert.Equal("sample-offset", timeZoneOption.FormattedOffset);
-        Assert.Equal("sample-city", timeZoneOption.City);
-    }
-
-    #endregion
-
     #region Description
 
     [Fact]
--- src/Buttercup.Web/Localization/TimeZoneOption.cs
+++ src/Buttercup.Web/Localization/TimeZoneOption.cs
@@ -3,64 +3,21 @@ namespace Buttercup.Web.Localization;
 /// <summary>
 /// Represents a time zone option.
 /// </summary>
-public sealed class TimeZoneOption
+/// <param name="Id">
+/// The time zone ID.
+/// </param>
+/// <param name="CurrentOffset">
+/// The time zone's current UTC offset.
+/// </param>
+/// <param name="FormattedOffset">
+/// A localized string representation of the time zone's current UTC offset.
+/// </param>
+/// <param name="City">
+/// The localized name of the time zone's exemplar city.
+/// </param>
+public sealed record TimeZoneOption(
+    string Id, TimeSpan CurrentOffset, string FormattedOffset, string City)
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="TimeZoneOption"/> class.
-    /// </summary>
-    /// <param name="id">
-    /// The time zone ID.
-    /// </param>
-    /// <param name="currentOffset">
-    /// The time zone's current UTC offset.
-    /// </param>
-    /// <param name="formattedOffset">
-    /// A localized string representation of the time zone's current UTC offset.
-    /// </param>
-    /// <param name="city">
-    /// The localized name of the time zone's exemplar city.
-    /// </param>
-    public TimeZoneOption(
-        string id, TimeSpan currentOffset, string formattedOffset, string city)
-    {
-        this.Id = id;
-        this.CurrentOffset = currentOffset;
-        this.FormattedOffset = formattedOffset;
-        this.City = city;
-    }
-
-    /// <summary>
-    /// Gets the time zone ID.
-    /// </summary>
-    /// <value>
-    /// The time zone ID.
-    /// </value>
-    public string Id { get; }
-
-    /// <summary>
-    /// Gets the time zone's current UTC offset.
-    /// </summary>
-    /// <value>
-    /// The time zone's current UTC offset.
-    /// </value>
-    public TimeSpan CurrentOffset { get; }
-
-    /// <summary>
-    /// Gets a localized string representation of the time zone's current UTC offset.
-    /// </summary>
-    /// <value>
-    /// A localized string representation of the time zone's current UTC offset.
-    /// </value>
-    public string FormattedOffset { get; }
-
-    /// <summary>
-    /// Gets the localized name of the time zone's exemplar city.
-    /// </summary>
-    /// <value>
-    /// The localized name of the time zone's exemplar city.
-    /// </value>
-    public string City { get; }
-
     /// <summary>
     /// Gets a localized description of the time zone.
     /// </summary>
