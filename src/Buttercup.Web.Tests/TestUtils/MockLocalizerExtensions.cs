29f03ea1fdd5c3bef83b06d89f1534ba23fe4df0
Stephen Feest <steve84@gmail.com>
2023-06-13 13:51:39 +0100

    Add XML documentation to `MockLocalizerExtensions`

--- src/Buttercup.Web.Tests/TestUtils/MockLocalizerExtensions.cs
+++ src/Buttercup.Web.Tests/TestUtils/MockLocalizerExtensions.cs
@@ -3,8 +3,30 @@ using Moq;
 
 namespace Buttercup.Web.TestUtils;
 
+/// <summary>
+/// Provides extension methods for string localizer mocks.
+/// </summary>
 internal static class MockLocalizerExtensions
 {
+    /// <summary>
+    /// Specifies the string that should be returned when a named resource is requested without
+    /// arguments.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type that the localizer provides strings for.
+    /// </typeparam>
+    /// <param name="mockLocalizer">
+    /// The mock localizer.
+    /// </param>
+    /// <param name="name">
+    /// The resource name.
+    /// </param>
+    /// <param name="translatedString">
+    /// The translated resource string.
+    /// </param>
+    /// <returns>
+    /// The mock localizer, for chaining.
+    /// </returns>
     public static Mock<IStringLocalizer<T>> SetupLocalizedString<T>(
         this Mock<IStringLocalizer<T>> mockLocalizer, string name, string translatedString)
     {
@@ -15,6 +37,28 @@ internal static class MockLocalizerExtensions
         return mockLocalizer;
     }
 
+    /// <summary>
+    /// Specifies the string that should be returned when a named resource is requested with
+    /// specific arguments.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type that the localizer provides strings for.
+    /// </typeparam>
+    /// <param name="mockLocalizer">
+    /// The mock localizer.
+    /// </param>
+    /// <param name="name">
+    /// The resource name.
+    /// </param>
+    /// <param name="arguments">
+    /// The string arguments.
+    /// </param>
+    /// <param name="translatedString">
+    /// The translated string.
+    /// </param>
+    /// <returns>
+    /// The mock localizer, for chaining.
+    /// </returns>
     public static Mock<IStringLocalizer<T>> SetupLocalizedString<T>(
         this Mock<IStringLocalizer<T>> mockLocalizer,
         string name,
