51b88926bd6d661e6a21e31e4aef6e9ff839e608
Stephen Feest <steve84@gmail.com>
2023-06-14 12:58:20 +0100

    Add XML documentation to `CustomClaimTypes`

--- src/Buttercup.Web/Authentication/CustomClaimTypes.cs
+++ src/Buttercup.Web/Authentication/CustomClaimTypes.cs
@@ -1,6 +1,12 @@
 namespace Buttercup.Web.Authentication;
 
+/// <summary>
+/// Provides constants for custom claim types.
+/// </summary>
 public static class CustomClaimTypes
 {
+    /// <summary>
+    /// The URI for a claim that specifies the security stamp for an entity.
+    /// </summary>
     public const string SecurityStamp = "http://schemas.smf.me.uk/buttercup/security-stamp";
 }
