4e00eecc33548435b75cb4bad38018e16493e1b4
Stephen Feest <steve84@gmail.com>
2023-11-12 22:23:34 +0000

    Remove redundant user data provider methods
    `PasswordAuthenticationService` was the only consumer of these methods.
    It has now been refactored to call the Entity Framework directly
    instead.

--- src/Buttercup.DataAccess/IUserDataProvider.cs
+++ src/Buttercup.DataAccess/IUserDataProvider.cs
@@ -8,21 +8,6 @@ namespace Buttercup.DataAccess;
 public interface IUserDataProvider
 {
     /// <summary>
-    /// Tries to find a user by email address.
-    /// </summary>
-    /// <param name="dbContext">
-    /// The database context.
-    /// </param>
-    /// <param name="email">
-    /// The email address.
-    /// </param>
-    /// <returns>
-    /// A task for the operation. The result is the user, or a null reference if no matching
-    /// user is found.
-    /// </returns>
-    Task<User?> FindUserByEmail(AppDbContext dbContext, string email);
-
-    /// <summary>
     /// Gets a user.
     /// </summary>
     /// <param name="dbContext">
@@ -38,60 +23,6 @@ public interface IUserDataProvider
     /// No matching user was found.
     /// </exception>
     Task<User> GetUser(AppDbContext dbContext, long id);
-
-    /// <summary>
-    /// Updates the user attributes affected by a password change.
-    /// </summary>
-    /// <param name="dbContext">
-    /// The database context.
-    /// </param>
-    /// <param name="userId">
-    /// The user ID.
-    /// </param>
-    /// <param name="hashedPassword">
-    /// The hashed password.
-    /// </param>
-    /// <param name="securityStamp">
-    /// The new security stamp.
-    /// </param>
-    /// <returns>
-    /// A task for the operation.
-    /// </returns>
-    /// <exception cref="NotFoundException">
-    /// No matching user was found.
-    /// </exception>
-    Task SaveNewPassword(
-        AppDbContext dbContext, long userId, string hashedPassword, string securityStamp);
-
-    /// <summary>
-    /// Updates the user attributes affected by the rehashing of an existing password.
-    /// </summary>
-    /// <param name="dbContext">
-    /// The database context.
-    /// </param>
-    /// <param name="userId">
-    /// The user ID.
-    /// </param>
-    /// <param name="baseRevision">
-    /// The base revision. Used for concurrency checking.
-    /// </param>
-    /// <param name="rehashedPassword">
-    /// The rehashed password.
-    /// </param>
-    /// <param name="timestamp">
-    /// The timestamp for the operation.
-    /// </param>
-    /// <returns>
-    /// A task for the operation. The result is <b>true</b> if the user was updated, <b>false</b> if
-    /// the user does not exist or <paramref name="baseRevision"/> does not match the revision
-    /// currently in the database.
-    /// </returns>
-    Task<bool> SaveRehashedPassword(
-        AppDbContext dbContext,
-        long userId,
-        int baseRevision,
-        string rehashedPassword,
-        DateTime timestamp);
 
     /// <summary>
     /// Sets a user's time zone.
--- src/Buttercup.DataAccess/UserDataProvider.cs
+++ src/Buttercup.DataAccess/UserDataProvider.cs
@@ -11,40 +11,8 @@ internal sealed class UserDataProvider : IUserDataProvider
 
     public UserDataProvider(IClock clock) => this.clock = clock;
 
-    public Task<User?> FindUserByEmail(AppDbContext dbContext, string email) =>
-        dbContext.Users.SingleOrDefaultAsync(u => u.Email == email);
-
     public async Task<User> GetUser(AppDbContext dbContext, long id) =>
         await dbContext.Users.FindAsync(id) ?? throw UserNotFound(id);
-
-    public Task SaveNewPassword(
-        AppDbContext dbContext, long userId, string hashedPassword, string securityStamp) =>
-        UpdateUserProperties(
-            dbContext,
-            userId,
-            s => s.SetProperty(u => u.HashedPassword, hashedPassword)
-                .SetProperty(u => u.SecurityStamp, securityStamp)
-                .SetProperty(u => u.PasswordCreated, this.clock.UtcNow)
-                .SetProperty(u => u.Modified, this.clock.UtcNow)
-                .SetProperty(u => u.Revision, u => u.Revision + 1));
-
-    public async Task<bool> SaveRehashedPassword(
-        AppDbContext dbContext,
-        long userId,
-        int baseRevision,
-        string rehashedPassword,
-        DateTime timestamp)
-    {
-        var updatedRows = await dbContext
-            .Users
-            .Where(u => u.Id == userId && u.Revision == baseRevision)
-            .ExecuteUpdateAsync(
-                s => s.SetProperty(u => u.HashedPassword, rehashedPassword)
-                    .SetProperty(u => u.Modified, timestamp)
-                    .SetProperty(u => u.Revision, u => u.Revision + 1));
-
-        return updatedRows > 0;
-    }
 
     public Task SetTimeZone(AppDbContext dbContext, long userId, string timeZone) =>
         UpdateUserProperties(
