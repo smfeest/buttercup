6a8ab4ef9ba690efcdce8d37dbfa042be7dca73e
Stephen Feest <steve84@gmail.com>
2023-11-15 22:21:19 +0000

    Add users page to admin area

---
+++ src/Buttercup.Web.Tests/Areas/Admin/Controllers/UsersControllerTests.cs
@@ -0,0 +1,40 @@
+using Buttercup.TestUtils;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Buttercup.Web.Areas.Admin.Controllers;
+
+public sealed class UsersControllerTests(DatabaseFixture<UsersControllerTests> databaseFixture)
+    : DatabaseTests<UsersControllerTests>(databaseFixture),
+    IClassFixture<DatabaseFixture<UsersControllerTests>>,
+    IDisposable
+{
+    private readonly ModelFactory modelFactory = new();
+
+    private readonly UsersController usersController = new(databaseFixture);
+
+    public void Dispose() => this.usersController.Dispose();
+
+    #region Index
+
+    [Fact]
+    public async Task Index_ReturnsViewResultWithUsersOrderedByNameAndEmail()
+    {
+        var orderedUsers = new[]
+        {
+            this.modelFactory.BuildUser() with { Name = "Anna", Email = "w@example.com" },
+            this.modelFactory.BuildUser() with { Name = "Anna", Email = "z@example.com" },
+            this.modelFactory.BuildUser() with { Name = "Bob", Email = "x@example.com" },
+            this.modelFactory.BuildUser() with { Name = "Clara", Email = "y@example.com" },
+        };
+
+        await this.DatabaseFixture.InsertEntities(
+            orderedUsers[1], orderedUsers[3], orderedUsers[2], orderedUsers[0]);
+
+        var result = Assert.IsType<ViewResult>(await this.usersController.Index());
+
+        Assert.Equal(orderedUsers, result.Model);
+    }
+
+    #endregion
+}
---
+++ src/Buttercup.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+using Buttercup.EntityModel;
+using Buttercup.Web.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buttercup.Web.Areas.Admin.Controllers;
+
+[Authorize(Policy = AuthorizationPolicyNames.AdminOnly)]
+[Area("Admin")]
+[Route("[area]/[controller]")]
+public sealed class UsersController(IDbContextFactory<AppDbContext> dbContextFactory) : Controller
+{
+    private readonly IDbContextFactory<AppDbContext> dbContextFactory = dbContextFactory;
+
+    [HttpGet]
+    public async Task<ViewResult> Index()
+    {
+        using var dbContext = this.dbContextFactory.CreateDbContext();
+        var users = await dbContext.Users.OrderBy(u => u.Name).ThenBy(u => u.Email).ToArrayAsync();
+        return this.View(users);
+    }
+}
