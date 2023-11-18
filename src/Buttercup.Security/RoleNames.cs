478b9cb719d56cca5c1229bb137f4dd05c83be1b
Stephen Feest <steve84@gmail.com>
2023-11-18 14:40:26 +0000

    Add constant for the admin role name

---
+++ src/Buttercup.Security/RoleNames.cs
@@ -0,0 +1,12 @@
+namespace Buttercup.Security;
+
+/// <summary>
+/// Provides constants for security role names.
+/// </summary>
+public static class RoleNames
+{
+    /// <summary>
+    /// The name of the admin role.
+    /// </summary>
+    public const string Admin = "Admin";
+}
