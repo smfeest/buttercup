8cfb089779a6e19192922d867ca03d65e7bedd4a
Stephen Feest <steve84@gmail.com>
2023-11-18 14:58:00 +0000

    Add admin role to principal when user is an admin

--- src/Buttercup.Security.Tests/UserPrincipalFactoryTests.cs
+++ src/Buttercup.Security.Tests/UserPrincipalFactoryTests.cs
@@ -10,54 +10,83 @@ public sealed class UserPrincipalFactoryTests
 {
     private const string AuthenticationType = "sample-authentication-type";
 
-    private readonly User user = new ModelFactory().BuildUser();
+    private readonly ModelFactory modelFactory = new();
 
     [Fact]
-    public void PrincipalHasNameIdClaim() => Assert.Equal(
-        this.user.Id.ToString(CultureInfo.InvariantCulture),
-        this.CreatePrincipal().FindFirstValue(ClaimTypes.NameIdentifier));
-
-    [Fact]
-    public void PrincipalHasNameClaim() =>
-        Assert.Equal(this.user.Name, this.CreatePrincipal().FindFirstValue(ClaimTypes.Name));
-
-    [Fact]
-    public void PrincipalHasEmailClaim() =>
-        Assert.Equal(this.user.Email, this.CreatePrincipal().FindFirstValue(ClaimTypes.Email));
-
-    [Fact]
-    public void PrincipalHasSecurityStampClaim() =>
+    public void PrincipalHasNameIdClaim()
+    {
+        var user = this.modelFactory.BuildUser();
         Assert.Equal(
-            this.user.SecurityStamp,
-            this.CreatePrincipal().FindFirstValue(CustomClaimTypes.SecurityStamp));
+            user.Id.ToString(CultureInfo.InvariantCulture),
+            this.CreatePrincipal(user).FindFirstValue(ClaimTypes.NameIdentifier));
+    }
 
     [Fact]
-    public void PrincipalHasTimeZoneClaim() =>
-        Assert.Equal(
-            this.user.TimeZone, this.CreatePrincipal().FindFirstValue(CustomClaimTypes.TimeZone));
+    public void PrincipalHasNameClaim()
+    {
+        var user = this.modelFactory.BuildUser();
+        Assert.Equal(user.Name, this.CreatePrincipal(user).FindFirstValue(ClaimTypes.Name));
+    }
 
     [Fact]
-    public void PrincipalHasRevisionClaim() =>
+    public void PrincipalHasEmailClaim()
+    {
+        var user = this.modelFactory.BuildUser();
+        Assert.Equal(user.Email, this.CreatePrincipal(user).FindFirstValue(ClaimTypes.Email));
+    }
+
+    [Fact]
+    public void PrincipalHasSecurityStampClaim()
+    {
+        var user = this.modelFactory.BuildUser();
         Assert.Equal(
-            this.user.Revision.ToString(CultureInfo.InvariantCulture),
-            this.CreatePrincipal().FindFirstValue(CustomClaimTypes.UserRevision));
+            user.SecurityStamp,
+            this.CreatePrincipal(user).FindFirstValue(CustomClaimTypes.SecurityStamp));
+    }
+
+    [Fact]
+    public void PrincipalHasTimeZoneClaim()
+    {
+        var user = this.modelFactory.BuildUser();
+        Assert.Equal(
+            user.TimeZone, this.CreatePrincipal(user).FindFirstValue(CustomClaimTypes.TimeZone));
+    }
+
+    [Fact]
+    public void PrincipalHasRevisionClaim()
+    {
+        var user = this.modelFactory.BuildUser();
+        Assert.Equal(
+            user.Revision.ToString(CultureInfo.InvariantCulture),
+            this.CreatePrincipal(user).FindFirstValue(CustomClaimTypes.UserRevision));
+    }
 
     [Fact]
     public void PrincipalHasAuthenticationType()
     {
-        var identity = this.CreatePrincipal().Identity;
+        var identity = this.CreatePrincipal(this.modelFactory.BuildUser()).Identity;
         Assert.NotNull(identity);
         Assert.Equal(AuthenticationType, identity.AuthenticationType);
+    }
+
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void PrincipalHasAdminRoleIfUserIsAdmin(bool userIsAdmin)
+    {
+        var user = this.modelFactory.BuildUser() with { IsAdmin = userIsAdmin };
+        Assert.Equal(userIsAdmin, this.CreatePrincipal(user).IsInRole(RoleNames.Admin));
     }
 
     [Fact]
     public void IdentityGetsNameFromNameClaim()
     {
-        var identity = this.CreatePrincipal().Identity;
+        var user = this.modelFactory.BuildUser();
+        var identity = this.CreatePrincipal(user).Identity;
         Assert.NotNull(identity);
-        Assert.Equal(this.user.Name, identity.Name);
+        Assert.Equal(user.Name, identity.Name);
     }
 
-    private ClaimsPrincipal CreatePrincipal() =>
-        new UserPrincipalFactory().Create(this.user, AuthenticationType);
+    private ClaimsPrincipal CreatePrincipal(User user) =>
+        new UserPrincipalFactory().Create(user, AuthenticationType);
 }
