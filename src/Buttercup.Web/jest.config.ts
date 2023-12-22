73b21d4cc60bfa1844ffdf1e8706e89d7bf50374
Stephen Feest <steve84@gmail.com>
2023-12-22 15:27:30 +0000

    Skip `e2e` directory when running Jest tests
    Since Jest and Playwright both match files named `*.spec.ts` by default,
    before setting up Playwright, we need to configure Jest to ignore the
    `e2e` directory that'll contain the Playwright tests.

--- src/Buttercup.Web/jest.config.ts
+++ src/Buttercup.Web/jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['e2e'],
 };
 
 export default config;
