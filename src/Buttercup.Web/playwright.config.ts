cf7a23975c265af7e0be6bd51c21cdd2bcd70df9
Stephen Feest <steve84@gmail.com>
2023-12-22 14:30:14 +0000

    Set base URL for end-to-end tests

--- src/Buttercup.Web/playwright.config.ts
+++ src/Buttercup.Web/playwright.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
+    baseURL: 'http://localhost:5000',
     trace: 'on-first-retry',
   },
   projects: [
