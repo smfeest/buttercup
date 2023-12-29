b8ed238228af1bbf6f9b15d09984adaff215c463
Stephen Feest <steve84@gmail.com>
2023-12-29 08:19:14 +0000

    Disable retrying of end-to-end tests in CI
    The config file created by the Playwright initializer enables up to two
    retries per test in CI. This can save time when tests are failing
    intermittently for reasons outside our control but, conversely, can
    significantly increase the running time of consistently failing tests.
    
    I'd therefore prefer to disable retries until we know that we need
    them.

--- src/Buttercup.Web/playwright.config.ts
+++ src/Buttercup.Web/playwright.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   use: {
     baseURL,
