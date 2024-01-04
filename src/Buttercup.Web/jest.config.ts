c8c93aebdb920c021a09af0c74407c58af3833c8
Stephen Feest <steve84@gmail.com>
2024-01-04 22:01:41 +0000

    Configure Jest to only search for tests in `scripts` directory
    Because Jest and Playwright both match files named `*.spec.ts` by
    default, we need to be more specific about which files Jest should
    search for tests.
    
    There are a number of ways we could have done such as using the
    `testPathIgnorePatterns` option to exclude directories containing
    Playwright tests.
    
    However, given that all our Jest specs will be named `*.spec.ts` and
    located within the `scripts/` directory, I decided that the simplest and
    most elegant solution was to override the `testMatch` pattern [1]
    accordingly.
    
    [1] https://jestjs.io/docs/configuration#testmatch-arraystring

--- src/Buttercup.Web/jest.config.ts
+++ src/Buttercup.Web/jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/scripts/**/*.spec.ts'],
 };
 
 export default config;
