8052c7caa2b7d673c1f15eceb6cc8f151f43ea14
Stephen Feest <steve84@gmail.com>
2024-01-11 22:14:56 +0000

    Add end-to-end test for viewing account details
    I've not included tests for changing the user's password or preferences.
    Doing so reliably with tests running in parallel would require a
    separate user account per worker process which – given that the
    application doesn't currently provide any interface for creating users –
    is something I'm deferring to a future branch.

--- src/Buttercup.Web/e2e/helpers/navigation.ts
+++ src/Buttercup.Web/e2e/helpers/navigation.ts
@@ -4,5 +4,8 @@ export class Navigation {
   public constructor(public readonly page: Page) {}
 
   public menuButton = this.page.getByRole('button', { name: 'Toggle menu' });
+  public yourAccountLink = this.page.getByRole('link', {
+    name: 'Your account',
+  });
   public signOutLink = this.page.getByRole('link', { name: 'Sign out' });
 }
