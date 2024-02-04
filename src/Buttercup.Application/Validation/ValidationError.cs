dbddd44ba9860e2e889857f96cb45f831e12e74b
Stephen Feest <steve84@gmail.com>
2024-02-04 10:07:29 +0000

    Add class to represent a validation error
    This class will be used by a new `IValidator<T>` service that will be added
    in the next commit.
    
    It will serve a similar purpose to the `ValidationResult` in
    `System.ComponentModel.DataAnnotations`, with a few notable differences:
    
     - This class includes an `Instance` property so that validation errors
       can be matched to a specific object instance when validation errors
       from multiple objects are added to a single collection. This could be
     - This class has a string-valued `Member` property instead of an
       enumerable `MemberNames` property. There is therefore an assumption
       that each validation error is associated with at most one property.
     - This class includes a `ValidationAttribute` property so any
       properties on the validation attribute that produced the error are
       available when the error is eventually handled or presented.

---
+++ src/Buttercup.Application/Validation/ValidationError.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Buttercup.Application.Validation;
+
+/// <summary>
+/// Represents a validation error.
+/// </summary>
+/// <param name="Message">
+/// The message.
+/// </param>
+/// <param name="Instance">
+/// The object that failed validation.
+/// </param>
+/// <param name="Member">
+/// The name of the property that failed validation, or null if validation failed at object level.
+/// </param>
+/// <param name="Value">
+/// The value that failed validation.
+/// </param>
+/// <param name="ValidationAttribute">
+/// The validation attribute that raised the error.
+/// </param>
+public record ValidationError(
+    string Message,
+    object Instance,
+    string? Member,
+    object? Value,
+    ValidationAttribute ValidationAttribute);
