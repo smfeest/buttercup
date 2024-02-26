23f27afc9cbd463739746ea98926dc76ddd25358
Stephen Feest <steve84@gmail.com>
2024-02-26 21:13:56 +0000

    Add GraphQL type to represent a validation error
    Although the GraphQL type is named `ValidationError`, as this class will
    be used alongside `Buttercup.Application.Validation.ValidationError`,
    I've named the runtime type `InputObjectValidationError`.
    
    The values in the `ValidationErrorCode` enum correspond to the
    validation attributes currently in use in the `RecipeAttributes` class.
    Additional codes can be added as and when they are needed.

---
+++ src/Buttercup.Web/Api/ValidationErrorCode.cs
@@ -0,0 +1,20 @@
+namespace Buttercup.Web.Api;
+
+/// <summary>
+/// Defines the error codes for validation errors.
+/// </summary>
+public enum ValidationErrorCode
+{
+    /// <summary>
+    /// Indicates that the string value provided is too long or too short for the field.
+    /// </summary>
+    InvalidStringLength,
+    /// <summary>
+    /// Indicates that the value provided is out of range for the field.
+    /// </summary>
+    OutOfRange,
+    /// <summary>
+    /// Indicates that the field requires a value and none was provided.
+    /// </summary>
+    Required,
+}
