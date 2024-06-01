2258973a805430c493095b590e1e970cd360f0fa
Stephen Feest <steve84@gmail.com>
2024-06-01 21:31:24 +0100

    Use `nameof` expression for role name
    This helps ensure consistency between the role name constant and value.

--- src/Buttercup.Security/RoleNames.cs
+++ src/Buttercup.Security/RoleNames.cs
@@ -8,5 +8,5 @@ public static class RoleNames
     /// <summary>
     /// The name of the admin role.
     /// </summary>
-    public const string Admin = "Admin";
+    public const string Admin = nameof(Admin);
 }
