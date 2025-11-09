f2aac80cc5dd754496eeb68508bb85549328360b
Stephen Feest <steve84@gmail.com>
2025-11-09 22:20:48 +0000

    Convert sign in form helper into a fixture

--- src/Buttercup.Web/e2e/sign-in.spec.ts
+++ src/Buttercup.Web/e2e/sign-in.spec.ts
@@ -1,13 +1,15 @@
 import { expect } from '@playwright/test';
-import { SignInForm } from './helpers/sign-in-form';
 import { test } from './test';
 
 const userEmail = 'e2e-user@example.com';
 
-test('can sign in with valid credentials', async ({ page, navigation }) => {
+test('can sign in with valid credentials', async ({
+  page,
+  navigation,
+  signInForm,
+}) => {
   await page.goto('/sign-in');
 
-  const signInForm = new SignInForm(page);
   await signInForm.emailInput.fill(userEmail);
   await signInForm.passwordInput.fill('e2e-user-pass');
   await signInForm.signInButton.click();
@@ -17,10 +19,9 @@ test('can sign in with valid credentials', async ({ page, navigation }) => {
   await expect(page.getByText('Signed in as E2E User')).toBeVisible();
 });
 
-test('cannot sign in with incorrect password', async ({ page }) => {
+test('cannot sign in with incorrect password', async ({ page, signInForm }) => {
   await page.goto('/sign-in');
 
-  const signInForm = new SignInForm(page);
   await signInForm.emailInput.fill(userEmail);
   await signInForm.passwordInput.fill('incorrect-password');
   await signInForm.signInButton.click();
@@ -30,10 +31,12 @@ test('cannot sign in with incorrect password', async ({ page }) => {
 });
 
 test.describe('when redirected to sign in from protected page', () => {
-  test('is returned to requested page after signing in', async ({ page }) => {
+  test('is returned to requested page after signing in', async ({
+    page,
+    signInForm,
+  }) => {
     await page.goto('/account');
 
-    const signInForm = new SignInForm(page);
     await signInForm.emailInput.fill(userEmail);
     await signInForm.passwordInput.fill('e2e-user-pass');
     await signInForm.signInButton.click();
--- src/Buttercup.Web/e2e/test.ts
+++ src/Buttercup.Web/e2e/test.ts
@@ -2,13 +2,16 @@ import { test as base } from '@playwright/test';
 import { api, Api } from './fixtures/api';
 import { navigation, Navigation } from './fixtures/navigation';
 import { recipeForm, RecipeForm } from './fixtures/recipe-form';
+import { signInForm, SignInForm } from './fixtures/sign-in-form';
 
 export const test = base.extend<{
   api: Api;
   navigation: Navigation;
   recipeForm: RecipeForm;
+  signInForm: SignInForm;
 }>({
   api,
   navigation,
   recipeForm,
+  signInForm,
 });
