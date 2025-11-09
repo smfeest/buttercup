2ca3c90d0c2b3cbeeba78131ae143caf639bb13e
Stephen Feest <steve84@gmail.com>
2025-11-09 22:01:45 +0000

    Convert recipe form helper into a fixture

--- src/Buttercup.Web/e2e/test.ts
+++ src/Buttercup.Web/e2e/test.ts
@@ -1,11 +1,14 @@
 import { test as base } from '@playwright/test';
 import { api, ApiFixture } from './fixtures/api';
 import { navigation, NavigationFixture } from './fixtures/navigation';
+import { recipeForm, RecipeFormFixture } from './fixtures/recipe-form';
 
 export const test = base.extend<{
   api: ApiFixture;
   navigation: NavigationFixture;
+  recipeForm: RecipeFormFixture;
 }>({
   api: ({ baseURL }, use) => use(api(baseURL)),
   navigation: ({ page }, use) => use(navigation(page)),
+  recipeForm: ({ page }, use) => use(recipeForm(page)),
 });
