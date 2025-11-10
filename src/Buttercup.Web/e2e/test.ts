f481bb2a28aa6bd70a8069114b36867f8de830a5
Stephen Feest <steve84@gmail.com>
2025-11-10 21:59:03 +0000

    Simplify API fixture definition

--- src/Buttercup.Web/e2e/test.ts
+++ src/Buttercup.Web/e2e/test.ts
@@ -1,10 +1,6 @@
 import { test as base } from '@playwright/test';
-import { api } from './fixtures/api';
+import { api, Api } from './fixtures/api';
 
-export const test = base.extend<{
-  api: (username: string) => ReturnType<typeof api>;
-}>({
-  async api({ baseURL }, use) {
-    await use((username) => api(baseURL!, username));
-  },
+export const test = base.extend<{ api: Api }>({
+  api,
 });
