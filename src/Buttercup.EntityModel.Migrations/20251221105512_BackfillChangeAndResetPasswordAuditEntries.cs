b1c1d98b3c5b30f86a7f33db9de33275a90965f1
Stephen Feest <steve84@gmail.com>
2025-12-21 11:52:01 +0000

    Add migration to backfill change and reset password audit entries
    With the application code changes to log password change and reset
    events as user audit entries instead of security events now deployed, we
    can safely backfill the `user_audit_entries` table with earlier password
    changes and resets that were recorded as `password_change_success` and
    `password_reset_success` security events.
    
    I originally planned to delete the `password_change_success` and
    `password_reset_success` security events after conversion. However, I
    eventually decided against it as that would introduce gaps in the
    security event IDs and prevent us from checking for gaps in the IDs to
    detect tampering in the future.

---
+++ src/Buttercup.EntityModel.Migrations/20251221105512_BackfillChangeAndResetPasswordAuditEntries.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buttercup.EntityModel.Migrations;
+
+public partial class BackfillChangeAndResetPasswordAuditEntries : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Sql(
+        @"INSERT INTO user_audit_entries (time, operation_type, target_id, actor_id, ip_address)
+        SELECT time, 'change_password', user_id, user_id, ip_address
+        FROM security_events
+        WHERE event = 'password_change_success';
+
+        INSERT INTO user_audit_entries (time, operation_type, target_id, actor_id, ip_address)
+        SELECT time, 'reset_password', user_id, user_id, ip_address
+        FROM security_events
+        WHERE event = 'password_reset_success';");
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
