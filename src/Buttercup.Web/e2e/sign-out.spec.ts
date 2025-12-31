8f1eba3b89ae595fb1ebc96cabd958609bf888fc
Stephen Feest <steve84@gmail.com>
2025-12-31 11:06:44 +0000

    Add end-to-end test for session invalidation on user deactivation

--- src/Buttercup.Web/e2e/sign-out.spec.ts
+++ src/Buttercup.Web/e2e/sign-out.spec.ts
@@ -12,3 +12,27 @@ test('can sign out', async ({ page, navigation }) => {
 
   await expect(page).toHaveTitle(/Sign in/);
 });
+
+test('user is signed out on deactivation', async ({
+  api,
+  page,
+  navigation,
+  session: { signIn },
+}) => {
+  const { createTestUser, deactivateUser, hardDeleteTestUser } =
+    api('e2e-admin');
+  const user = await createTestUser();
+
+  try {
+    await signIn(user.email, user.password);
+    await navigation.menuButton.click();
+    await expect(page.getByText(`Signed in as ${user.name}`)).toBeVisible();
+
+    await deactivateUser(user.id);
+
+    await navigation.yourAccountLink.click();
+    await expect(page).toHaveTitle(/Sign in/);
+  } finally {
+    await hardDeleteTestUser(user.id);
+  }
+});
