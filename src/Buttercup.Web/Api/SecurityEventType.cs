297727af780c88b24ad4d4aa22c58383d425f87b
Stephen Feest <steve84@gmail.com>
2026-01-02 17:29:58 +0000

    Remove security events field and associated types from GraphQL API
    This is in preparation for dropping the security events table.

--- src/Buttercup.Web/Api/SecurityEventType.cs
+++
@@ -1,9 +0,0 @@
-using Buttercup.EntityModel;
-
-namespace Buttercup.Web.Api;
-
-public class SecurityEventType : ObjectType<SecurityEvent>
-{
-    protected override void Configure(IObjectTypeDescriptor<SecurityEvent> descriptor) =>
-        descriptor.Ignore(u => u.UserId);
-}
