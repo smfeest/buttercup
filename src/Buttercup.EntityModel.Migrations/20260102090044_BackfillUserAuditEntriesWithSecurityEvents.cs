93ea317fa6e98d44a9dc32146a37d25275e888b7
Stephen Feest <steve84@gmail.com>
2026-01-02 10:15:58 +0000

    Add migration to backfill user audit entries from security events
    This migration cannot be safely made reversible without either:
    
    + Deleting the migrated security events as part of the 'up' migration
      (so that they are not migrated twice if the migration is reversed and
      then re-applied)
    + Maintaining a permanent record of which security events have already
      been migrated (again, so that they only migrated once)
    + Maintaining a permanent record of which user audit entries were
      backfilled by the migration (so that they can be deleted by the 'down'
      migration and recreated without duplication if the migration is
      re-applied)
    
    Given how unlikely it is that we'll need to reverse this migration in
    practice, I decided that none of those options were worth the overhead.
    I've therefore omitted the `Down` method, which makes the migration
    irreversible instead.
    
    If there does turn out to be a need to reverse the migration in
    development, then this can still be done by manually deleting the
    `__migrations_history` record and the user audit events that were
    backfilled.

---
+++ src/Buttercup.EntityModel.Migrations/20260102090044_BackfillUserAuditEntriesWithSecurityEvents.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buttercup.EntityModel.Migrations;
+
+public partial class BackfillUserAuditEntriesWithSecurityEvents : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Sql("""
+        CREATE TEMPORARY TABLE security_event_mappings (
+            event VARCHAR(50) PRIMARY KEY,
+            operation VARCHAR(30) NOT NULL,
+            failure VARCHAR(30)
+        );
+
+        INSERT INTO security_event_mappings (event, operation, failure) VALUES
+            ('access_token_issued', 'create_access_token', NULL),
+            ('authentication_failure:incorrect_password', 'authenticate_password', 'incorrect_password'),
+            ('authentication_failure:no_password_set', 'authenticate_password', 'no_password_set'),
+            ('authentication_failure:user_deactivated', 'authenticate_password', 'user_deactivated'),
+            ('authentication_success', 'authenticate_password', NULL),
+            ('password_change_failure:incorrect_password', 'change_password', 'incorrect_password'),
+            ('password_change_failure:no_password_set', 'change_password', 'no_password_set'),
+            ('password_change_success', 'change_password', NULL),
+            ('password_reset_failure:user_deactivated', 'reset_password', 'user_deactivated'),
+            ('password_reset_link_sent', 'create_password_reset_token', NULL),
+            ('password_reset_success', 'reset_password', NULL),
+            ('sign_in', 'sign_in', NULL),
+            ('sign_out', 'sign_out', NULL);
+
+        INSERT INTO user_audit_entries (time, operation, target_id, actor_id, ip_address, failure)
+        SELECT time, operation, user_id, user_id, ip_address, failure
+        FROM security_events
+        JOIN security_event_mappings ON security_events.event = security_event_mappings.event;
+
+        DROP TEMPORARY TABLE security_event_mappings;
+        """);
+}
