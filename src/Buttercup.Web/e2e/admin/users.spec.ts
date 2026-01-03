f6112773a2f84f3be98d71aa24ccdfc64b581ba6
Stephen Feest <steve84@gmail.com>
2026-01-03 20:42:15 +0000

    Add 'Add User' form
    For now I've hardcoded the default time zone to 'Europe/London'. I'll
    make this configurable through an app setting in the next commit.

--- src/Buttercup.Web/e2e/admin/users.spec.ts
+++ src/Buttercup.Web/e2e/admin/users.spec.ts
@@ -1,6 +1,8 @@
 import { expect } from '@playwright/test';
 import { authStatePath } from '../auth-state';
+import { randomString } from '../helpers/random';
 import { test } from '../test';
+import { gql } from '@urql/core';
 
 test.describe('when signed in as an admin user', () => {
   test.use({ storageState: authStatePath('e2e-admin') });
@@ -12,6 +14,44 @@ test.describe('when signed in as an admin user', () => {
     await navigation.usersLink.click();
 
     await expect(page.locator('tr')).toContainText(['E2E Admin', 'E2E User']);
+  });
+
+  test('can add a new user', async ({ api, page }) => {
+    const { client, hardDeleteTestUser } = api('e2e-admin');
+
+    await page.goto('/admin/users');
+    await page.getByRole('link', { name: 'Add user' }).click();
+
+    const email = `joe.bloggs.${randomString()}@example.com`;
+
+    await page.getByLabel('Name').fill('Joe Bloggs');
+    await page.getByLabel('Email').fill(email);
+    await page.getByLabel('Time zone').selectOption('GMT+09:00 - Tokyo');
+    await page.getByRole('button', { name: 'Add' }).click();
+
+    try {
+      await expect(page.getByRole('heading', { name: 'Users' })).toBeVisible();
+      await expect(page.getByRole('cell', { name: email })).toBeVisible();
+    } finally {
+      const QUERY_USER_QUERY = gql`
+        query ($email: String!) {
+          users(where: { email: { eq: $email } }) {
+            id
+          }
+        }
+      `;
+
+      const result = await client.query<{ users: { id: number }[] }>(
+        QUERY_USER_QUERY,
+        { email },
+      );
+
+      const userId = result.data?.users[0]?.id;
+
+      if (userId) {
+        await hardDeleteTestUser(userId);
+      }
+    }
   });
 });
 
