24d235af99ac4bab7432ba318ad2296f1f71df86
Stephen Feest <steve84@gmail.com>
2026-03-29 22:07:48 +0100

    Backfill role for existing administrators
    Now that the application is correctly setting the role for all new
    users, we can safely backfill the role for existing administrators who
    may still have the default of 'contributor'.
    
    This represents the third step towards removing the `IsAdmin` property
    on users in favour of `Role`.

---
+++ src/Buttercup.EntityModel.Migrations/20260329210237_BackfillRoleForExistingAdministrators.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buttercup.EntityModel.Migrations;
+
+public partial class BackfillRoleForExistingAdministrators : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) =>
+        migrationBuilder.Sql("UPDATE users SET role = 'admin' WHERE is_admin = 1");
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
