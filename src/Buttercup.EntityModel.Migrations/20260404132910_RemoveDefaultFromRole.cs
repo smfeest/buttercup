f2fd82f70520208ace0ed6a9208225cca529d425
Stephen Feest <steve84@gmail.com>
2026-04-04 15:38:09 +0100

    Remove default value from `role` column
    The application code is now explicitly setting the role when creating
    users so this database default is no longer required.

---
+++ src/Buttercup.EntityModel.Migrations/20260404132910_RemoveDefaultFromRole.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buttercup.EntityModel.Migrations;
+
+public partial class RemoveDefaultFromRole : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) =>
+        migrationBuilder
+            .AlterColumn<string>(
+                name: "role",
+                table: "users",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15,
+                oldDefaultValue: "contributor")
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+    protected override void Down(MigrationBuilder migrationBuilder) =>
+        migrationBuilder
+            .AlterColumn<string>(
+                name: "role",
+                table: "users",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "contributor",
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
+}
