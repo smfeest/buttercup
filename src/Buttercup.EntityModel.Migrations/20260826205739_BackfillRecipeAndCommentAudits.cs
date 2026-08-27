55140afc05ded7eae9cf6804a38f0961ad2393e3
Stephen Feest <steve84@gmail.com>
2026-08-27 09:37:16 +0100

    Add migration to backfill recipe and comment audits
    In production, the recipe revisions table was backfilled manually based
    on periodic database backups. This means that, in cases where a recipe
    was created and then updated between two database backups, there will be
    no revision corresponding to creation and instead revision 0 is
    associated with an update. This backfill migration therefore compares
    the `created` timestamps on the recipe and revision to determine whether
    revision 0 corresponds to a `create` or `update` action.
    
    Meanwhile, since the application does not yet allow the editing of
    comments, all existing comment revisions necessarily correspond to a
    `create` action.
    
    Since the application also doesn't yet support reverting recipes or
    comments to a previous revision, we can also safely assume that every
    existing revision should have exactly one matching audit record.
    
    Similarly, since the application doesn't yet support restoring
    soft-deleted records, we can safely assume that every soft-deleted
    comment and recipe should have exactly one delete audit.
    
    I considered various options for the reverse migration, including
    deleting all records from the audit tables (as they can easily be
    recreated by re-running the migration) and omitting the reverse
    migration completely to mark the migration as irreversible (like we did
    for the user audits backfill).
    
    In the end I decided to make the reverse migration a no-op. That's
    because, unlike the user audits backfill, this backfill does not modify
    the source records and is therefore essentially non-destructive. It
    therefore didn't feel appropriate to make it non-reversible. At the same
    time, leaving it a no-op gives the operator the option to either leave
    the backfilled records in place or manually clear the audit tables ready
    for a re-run.

---
+++ src/Buttercup.EntityModel.Migrations/20260826205739_BackfillRecipeAndCommentAudits.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buttercup.EntityModel.Migrations;
+
+public partial class BackfillRecipeAndCommentAudits : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Sql("""
+        INSERT INTO recipe_audits (recipe_id, time, action, revision_id, actor_id)
+        SELECT
+            recipes.id,
+            recipe_revisions.created,
+            IF(recipe_revisions.revision = 0 AND recipe_revisions.created = recipes.created, 'create', 'update'),
+            recipe_revisions.id,
+            recipe_revisions.created_by_user_id
+        FROM recipe_revisions
+        INNER JOIN recipes ON recipes.id = recipe_revisions.recipe_id
+        LEFT JOIN recipe_audits ON recipe_audits.revision_id = recipe_revisions.id
+        WHERE recipe_audits.id IS NULL;
+
+        INSERT INTO recipe_audits (recipe_id, time, action, actor_id)
+        SELECT recipes.id, recipes.deleted, 'delete', recipes.deleted_by_user_id
+        FROM recipes
+        LEFT JOIN recipe_audits
+            ON recipe_audits.recipe_id = recipes.id AND recipe_audits.action = 'delete'
+        WHERE recipes.deleted IS NOT NULL AND recipe_audits.id IS NULL;
+
+        INSERT INTO comment_audits (comment_id, time, action, revision_id, actor_id)
+        SELECT
+            comments.id,
+            comment_revisions.created,
+            'create',
+            comment_revisions.id,
+            comments.author_id
+        FROM comment_revisions
+        INNER JOIN comments ON comments.id = comment_revisions.comment_id
+        LEFT JOIN comment_audits ON comment_audits.revision_id = comment_revisions.id
+        WHERE comment_audits.id IS NULL;
+
+        INSERT INTO comment_audits (comment_id, time, action, actor_id)
+        SELECT comments.id, comments.deleted, 'delete', comments.deleted_by_user_id
+        FROM comments
+        LEFT JOIN comment_audits
+            ON comment_audits.comment_id = comments.id AND comment_audits.action = 'delete'
+        WHERE comments.deleted IS NOT NULL AND comment_audits.id IS NULL;
+        """);
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
