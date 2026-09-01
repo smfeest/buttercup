31b05f49ebf616b75205484c6fa95f085cb80b84
Stephen Feest <steve84@gmail.com>
2026-09-01 21:15:00 +0100

    Only allow administrators to view IP addresses on audits
    IP addresses are sensitive as they can be used to identify and geolocate
    individuals.
    
    That said, I think it's considered broadly acceptable to collect them in
    audit logs to facilitate the analysis of suspicious or disputed
    activity.
    
    For comparison, I took a brief look at what Wikipedia does with the IP
    addresses it records against edits (which I consider analogous to recipe
    and comment audits) and they currently have a similar policy of only
    exposing the recorded IP addresses to a small set of privileged
    'CheckUser' users and administrators [1].
    
    [1] https://en.wikipedia.org/wiki/Wikipedia:How_to_protect_your_privacy_on_Wikipedia_from_outside_interference#IP_address

--- src/Buttercup.Web/Api/CommentAuditType.cs
+++ src/Buttercup.Web/Api/CommentAuditType.cs
@@ -1,12 +1,17 @@
 using Buttercup.EntityModel;
+using Buttercup.Web.Security;
 
 namespace Buttercup.Web.Api;
 
 public sealed class CommentAuditType : ObjectType<CommentAudit>
 {
-    protected override void Configure(IObjectTypeDescriptor<CommentAudit> descriptor) =>
+    protected override void Configure(IObjectTypeDescriptor<CommentAudit> descriptor)
+    {
+        descriptor.Field(r => r.IpAddress).Authorize(AuthorizationPolicyNames.AdminOnly);
+
         descriptor
             .Ignore(r => r.CommentId)
             .Ignore(r => r.RevisionId)
             .Ignore(r => r.ActorId);
+    }
 }
--- src/Buttercup.Web/Api/RecipeAuditType.cs
+++ src/Buttercup.Web/Api/RecipeAuditType.cs
@@ -1,12 +1,17 @@
 using Buttercup.EntityModel;
+using Buttercup.Web.Security;
 
 namespace Buttercup.Web.Api;
 
 public sealed class RecipeAuditType : ObjectType<RecipeAudit>
 {
-    protected override void Configure(IObjectTypeDescriptor<RecipeAudit> descriptor) =>
+    protected override void Configure(IObjectTypeDescriptor<RecipeAudit> descriptor)
+    {
+        descriptor.Field(r => r.IpAddress).Authorize(AuthorizationPolicyNames.AdminOnly);
+
         descriptor
             .Ignore(r => r.RecipeId)
             .Ignore(r => r.RevisionId)
             .Ignore(r => r.ActorId);
+    }
 }
