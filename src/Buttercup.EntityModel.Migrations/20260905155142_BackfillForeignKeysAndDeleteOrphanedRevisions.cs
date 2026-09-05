c677dabfe91b7041e232612b2a5d0f7483a64c7e
Stephen Feest <steve84@gmail.com>
2026-09-05 17:10:53 +0100

    Backfill foreign keys and clean-up orphaned revisions
    This migration should not be run until the code changes to populate
    these columns for new records have been deployed.
    
    The delete statement is needed to clean-up orphaned revisions for any
    recipes and comments that were hard-deleted between the removal and
    re-instatement of these columns as there would have been no cascade
    delete during that period.

---
+++ src/Buttercup.EntityModel.Migrations/20260905155142_BackfillForeignKeysAndDeleteOrphanedRevisions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buttercup.EntityModel.Migrations;
+
+public partial class BackfillForeignKeysAndDeleteOrphanedRevisions : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Sql("""
+        UPDATE recipe_revisions, recipe_audits
+        SET recipe_revisions.recipe_id = recipe_audits.recipe_id
+        WHERE recipe_audits.revision_id = recipe_revisions.id;
+
+        DELETE recipe_revisions
+        FROM recipe_revisions
+        LEFT JOIN recipe_audits ON recipe_audits.revision_id = recipe_revisions.id
+        WHERE recipe_audits.id IS NULL;
+
+        UPDATE comment_revisions, comment_audits
+        SET comment_revisions.comment_id = comment_audits.comment_id
+        WHERE comment_audits.revision_id = comment_revisions.id;
+
+        DELETE comment_revisions
+        FROM comment_revisions
+        LEFT JOIN comment_audits ON comment_audits.revision_id = comment_revisions.id
+        WHERE comment_audits.id IS NULL;
+        """);
+}
