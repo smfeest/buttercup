0697439e541bda7b7fb4e7dd781c60fdca705061
Stephen Feest <steve84@gmail.com>
2026-09-06 21:49:22 +0100

    Backfill update count on recipes and comments
    Since the application does not yet support restoring soft-deleted
    records, we can simply set the update count to revision count + 1 for
    soft-deleted records, and equal to the revision count for all others.
    
    For safe no-downtime deployment, this backfill must be deployed _after_
    the application code changes that set the update count based on the same
    logic after every update and soft-deletion, and _before_ the application
    code changes that start using the update count for concurrency control.

---
+++ src/Buttercup.EntityModel.Migrations/20260906204236_BackfillUpdateCountOnRecipesAndComments.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buttercup.EntityModel.Migrations;
+
+public partial class BackfillUpdateCountOnRecipesAndComments : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Sql("""
+        UPDATE recipes
+        SET update_count = IF(deleted IS NULL, revision, revision + 1);
+
+        UPDATE comments
+        SET update_count = IF(deleted IS NULL, revision, revision + 1);
+        """);
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
