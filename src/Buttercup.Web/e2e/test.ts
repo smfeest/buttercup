26683ac94cf1548db88f3ffddd19331e50d29e1c
Stephen Feest <steve84@gmail.com>
2026-09-13 22:43:44 +0100

    Add end-to-end test coverage for editing comments

--- src/Buttercup.Web/e2e/test.ts
+++ src/Buttercup.Web/e2e/test.ts
@@ -4,15 +4,18 @@ import { navigation, Navigation } from './fixtures/navigation';
 import { recipeForm, RecipeForm } from './fixtures/recipe-form';
 import { session, Session } from './fixtures/session';
 import { signInForm, SignInForm } from './fixtures/sign-in-form';
+import { commentForm, CommentForm } from './fixtures/comment-form';
 
 export const test = base.extend<{
   api: Api;
+  commentForm: CommentForm;
   navigation: Navigation;
   recipeForm: RecipeForm;
   session: Session;
   signInForm: SignInForm;
 }>({
   api,
+  commentForm,
   navigation,
   recipeForm,
   session,
