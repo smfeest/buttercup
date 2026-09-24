68f42ee22893e8c88f1c03a27cc404e8ae51b869
Stephen Feest <steve84@gmail.com>
2026-09-24 09:26:12 +0100

    [skip ci] Add Gulp tasks for building icon sprite

--- src/Buttercup.Web/gulpfile.mjs
+++ src/Buttercup.Web/gulpfile.mjs
@@ -1,5 +1,7 @@
+import * as cheerio from 'cheerio';
 import cleanCss from 'gulp-clean-css';
 import { deleteAsync } from 'del';
+import fs from 'fs/promises';
 import gulp from 'gulp';
 import less from 'gulp-less';
 import rename from 'gulp-rename';
@@ -9,11 +11,43 @@ import webpackStream from 'webpack-stream';
 const { dest, parallel, series, src, watch } = gulp;
 
 const paths = {};
+paths.icons = 'icons';
 paths.scripts = 'scripts';
 paths.styles = 'styles';
 paths.assets = 'wwwroot/assets';
 paths.scriptAssets = `${paths.assets}/scripts`;
 paths.styleAssets = `${paths.assets}/styles`;
+
+const buildIcons = async () => {
+  const symbols = await Promise.all(
+    ['chef-hat', 'cooking-pot'].map(async (name) => {
+      const raw = await fs.readFile(
+        `node_modules/lucide-static/icons/${name}.svg`,
+        'utf8',
+      );
+      const $ = cheerio.load(raw, { xmlMode: true });
+
+      const $svg = $('svg');
+      const $symbol = $(`<symbol id="icon-${name}" />`);
+      const attributes = $svg.attr();
+
+      for (const name in attributes) {
+        if (!['class', 'height', 'width', 'xmlns'].includes(name)) {
+          $symbol.attr(name, attributes[name]);
+        }
+      }
+
+      $symbol.append($svg.children());
+
+      return $.xml($symbol);
+    }),
+  );
+
+  const sprite = `<svg xmlns="http://www.w3.org/2000/svg">\n${symbols.join('\n')}\n</svg>`;
+
+  await fs.mkdir(paths.icons, { recursive: true });
+  await fs.writeFile(`${paths.icons}/sprite.svg`, sprite);
+};
 
 const buildScriptsDev = () => webpackDevScripts().pipe(dest(paths.assets));
 
@@ -34,7 +68,14 @@ const buildStyles = () =>
     .pipe(dest(paths.styleAssets));
 
 const clean = () =>
-  deleteAsync([`${paths.scriptAssets}/**/*`, `${paths.styleAssets}/**/*`]);
+  deleteAsync([
+    `${paths.icons}/**/*`,
+    `${paths.scriptAssets}/**/*`,
+    `${paths.styleAssets}/**/*`,
+  ]);
+
+const watchIcons = () =>
+  watch('node_modules/lucide-static/icons/*.svg', buildIcons);
 
 const watchScripts = () =>
   webpackDevScripts({
@@ -79,15 +120,16 @@ const webpackScripts = (config) =>
     ),
   );
 
-const build = parallel(buildScripts, buildStyles);
+const build = parallel(buildIcons, buildScripts, buildStyles);
 
 const rebuild = series(clean, build);
 
-const watchAll = parallel(watchScripts, watchStyles);
+const watchAll = parallel(watchIcons, watchScripts, watchStyles);
 
 export {
   build as default,
   build,
+  buildIcons,
   buildScripts,
   buildScriptsDev,
   buildScriptsProd,
@@ -95,6 +137,7 @@ export {
   clean,
   rebuild,
   watchAll as watch,
+  watchIcons,
   watchScripts,
   watchStyles,
 };
